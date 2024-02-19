38c056f679ae2b761bddf771573fa6848b6547c5
Jason Heppler <jason.heppler@gmail.com>
2024-02-18 22:15:59 -0600

    Adding obsidian plugin to nvim

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,8 +5,15 @@
 -- Disable the concealing in some file formats
 -- The default conceallevel is 3 in LazyVim
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "json", "jsonc", "markdown" },
+  pattern = { "json", "jsonc" },
   callback = function()
     vim.wo.conceallevel = 0
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "markdown", "md" },
+  callback = function()
+    vim.wo.conceallevel = 2
   end,
 })
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,6 +1,69 @@
 return {
+  -- telescope file browser
   {
     "nvim-telescope/telescope-file-browser.nvim",
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+  },
+
+  -- obsidian
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "research",
+          path = "~/research/Research",
+        },
+      },
+    },
+    mappings = {
+      -- Toggle check-boxes.
+      ["<leader>ch"] = {
+        action = function()
+          return require("obsidian").util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+    },
+
+    note_id_func = function(title)
+      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+      -- In this case a note with the title 'My new note' will be given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+      local suffix = ""
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(os.time()) .. "-" .. suffix
+    end,
+
+    note_frontmatter_func = function(note)
+      -- if note id is empty, create a roam-style ID with a timestamp and a random suffix
+      if note.id == "" then
+        note.id = tostring(os.time())
+          .. "-"
+          .. string.char(math.random(65, 90))
+          .. string.char(math.random(65, 90))
+          .. string.char(math.random(65, 90))
+          .. string.char(math.random(65, 90))
+      end
+      return {
+        title = note.title,
+        id = note.id,
+        tags = note.tags,
+        date = os.date("%Y-%m-%d"),
+      }
+    end,
   },
 }
