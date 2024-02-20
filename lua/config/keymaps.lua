2372b814fdb8ae79d1adaf24086bbf2d75dc2ba6
Jason Heppler <jason.heppler@gmail.com>
2024-02-20 14:51:13 -0600

    Adding ToggleTerm support

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,29 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
--- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+-- Move line up and down
+keymap("n", "<A-j>", ":m .+1<CR>==", opts)
+keymap("n", "<A-k>", ":m .-2<CR>==", opts)
+
+-- obsidian
+-- when cursor is on a link, open it using :ObsidianFollowLink
+keymap("n", "<leader>of", ":ObsidianFollowLink<CR>", opts)
+-- show backlinks for the current note
+keymap("n", "<leader>ob", ":ObsidianBacklinks<CR>", opts)
+-- create today note
+keymap("n", "<leader>ot", ":ObsidianToday<CR>", opts)
+
+-- toggleterm
+-- fire off :ToggleTerm
+keymap("n", "<leader>tt", ":ToggleTerm<CR>", opts)
+
+function _G.set_terminal_keymaps()
+  local args = { buffer = 0 }
+  vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], args)
+  vim.keymap.set("t", "jk", [[<C-\><C-n>]], args)
+  vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], args)
+  vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], args)
+  vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], args)
+  vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], args)
+  vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], args)
+end
