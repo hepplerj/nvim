9c8c4ba2f8c7c513f85a5099b063a59e6a07dd04
Jason Heppler <jason.heppler@gmail.com>
2024-04-23 16:25:09 -0500

    style: Add Everforest to lazy load

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,8 +25,10 @@ require("lazy").setup({
     -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
+    -- set evergreen as the default color scheme
+    colorscheme = "evergreen",
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "everforest", "tokyonight", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
