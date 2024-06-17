3f6feacbf40da8fe14922058564a6aa63d8ba0cd
Jason Heppler <jason.heppler@gmail.com>
2024-06-16 21:45:48 -0500

    Add nvim orgmode for task capturing

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -14,4 +14,31 @@ return {
   {
     "mechatroner/rainbow_csv",
   },
+  -- nvim-orgmode
+  {
+    "nvim-orgmode/orgmode",
+    event = "VeryLazy",
+    ft = { "org" },
+    config = function()
+      -- Setup orgmode
+      require("orgmode").setup({
+        org_agenda_files = "~/org/**/*",
+        org_default_notes_file = "~/org/refile.org",
+      })
+
+      -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+      -- add ~org~ to ignore_install
+      -- require('nvim-treesitter.configs').setup({
+      --   ensure_installed = 'all',
+      --   ignore_install = { 'org' },
+      -- })
+    end,
+  },
+  -- org-bullets
+  {
+    "nvim-orgmode/org-bullets.nvim",
+    config = function()
+      require("org-bullets").setup()
+    end,
+  },
 }
