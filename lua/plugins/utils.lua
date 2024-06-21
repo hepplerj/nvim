f58f6361977f93a3adf958f116e1fa321fcf9d02
Jason Heppler <jason.heppler@gmail.com>
2024-06-21 10:38:40 -0500

    Setup for nvim orgmode

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -19,26 +19,42 @@ return {
     "nvim-orgmode/orgmode",
     event = "VeryLazy",
     ft = { "org" },
+    dependencies = { "nvim-orgmode/org-bullets.nvim" },
     config = function()
-      -- Setup orgmode
       require("orgmode").setup({
         org_agenda_files = "~/org/**/*",
         org_default_notes_file = "~/org/refile.org",
+        org_agenda_start_on_weekday = false,
+        org_startup_indented = true,
+        org_log_into_drawer = "LOGBOOK",
+        org_todo_keywords = { "TODO(t)", "PROGRESS(p)", "|", "DONE(d)", "REJECTED(r)", "WAITING(w)" },
+        org_capture_templates = {
+          t = {
+            description = "Refile",
+            template = "* TODO %?\nDEADLINE: %T",
+          },
+          T = {
+            description = "Todo",
+            template = "* TODO %?\nDEADLINE: %T",
+            target = "~/org/todos.org",
+          },
+          w = {
+            description = "Work todo",
+            template = "* TODO %?\nDEADLINE: %T",
+            target = "~/org/work.org",
+          },
+        },
       })
-
-      -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
-      -- add ~org~ to ignore_install
-      -- require('nvim-treesitter.configs').setup({
-      --   ensure_installed = 'all',
-      --   ignore_install = { 'org' },
-      -- })
-    end,
-  },
-  -- org-bullets
-  {
-    "nvim-orgmode/org-bullets.nvim",
-    config = function()
-      require("org-bullets").setup()
+      require("org-bullets").setup({
+        concealcursor = true,
+        symbols = {
+          checkboxes = {
+            half = { "", "@org.checkbox.halfchecked" },
+            done = { "✓", "@org.checkbox.checked" },
+            todo = { " ", "@org.checkbox" },
+          },
+        },
+      })
     end,
   },
 }
