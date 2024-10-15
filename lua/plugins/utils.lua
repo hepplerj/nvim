04f81c300a6261df5f6cef4d2518b5fb8b5a1226
Jason Heppler <jason.heppler@gmail.com>
2024-10-15 15:04:19 -0500

    feat: Updated orgmode plugin

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -1,15 +1,4 @@
 return {
-  -- lorem ipsum
-  {
-    "derektata/lorem.nvim",
-    config = function()
-      local lorem = require("lorem")
-      lorem.setup({
-        sentenceLength = "mixedShort",
-        comma = 1,
-      })
-    end,
-  },
   -- rainbow_csv
   {
     "mechatroner/rainbow_csv",
@@ -23,56 +12,56 @@ return {
     end,
   },
   -- nvim-orgmode
-  -- {
-  --   "nvim-orgmode/orgmode",
-  --   event = "VeryLazy",
-  --   ft = { "org" },
-  --   dependencies = { "nvim-orgmode/org-bullets.nvim" },
-  --   config = function()
-  --     require("orgmode").setup({
-  --       org_agenda_files = "~/Documents/org/**/*",
-  --       org_default_notes_file = "~/Documents/org/notes.org",
-  --       org_agenda_start_on_weekday = false,
-  --       org_startup_indented = true,
-  --       org_log_into_drawer = "LOGBOOK",
-  --       org_todo_keywords = {
-  --         "TODO(t)",
-  --         "NEXT(n)",
-  --         "IDEA(i)",
-  --         "HOLD(h)",
-  --         "REVW(r)",
-  --         "|",
-  --         "DONE(d)",
-  --         "REJECTED(r)",
-  --         "WAITING(w)",
-  --       },
-  --       org_capture_templates = {
-  --         t = {
-  --           description = "Refile",
-  --           template = "* TODO %?\nDEADLINE: %T",
-  --         },
-  --         T = {
-  --           description = "Todo",
-  --           template = "* TODO %?\nDEADLINE: %T",
-  --           target = "~/Documents/org/todos.org",
-  --         },
-  --         w = {
-  --           description = "Work todo",
-  --           template = "* TODO %?\nDEADLINE: %T",
-  --           target = "~/Documents/org/work.org",
-  --         },
-  --       },
-  --     })
-  --     require("org-bullets").setup({
-  --       concealcursor = true,
-  --       symbols = {
-  --         checkboxes = {
-  --           half = { "", "@org.checkbox.halfchecked" },
-  --           done = { "✓", "@org.checkbox.checked" },
-  --           todo = { " ", "@org.checkbox" },
-  --         },
-  --       },
-  --     })
-  --   end,
-  -- },
+  {
+    "nvim-orgmode/orgmode",
+    event = "VeryLazy",
+    ft = { "org" },
+    dependencies = { "nvim-orgmode/org-bullets.nvim" },
+    config = function()
+      require("orgmode").setup({
+        org_agenda_files = "~/Documents/org/**/*",
+        org_default_notes_file = "~/Documents/org/notes.org",
+        org_agenda_start_on_weekday = false,
+        org_startup_indented = true,
+        org_log_into_drawer = "LOGBOOK",
+        org_todo_keywords = {
+          "TODO(t)",
+          "NEXT(n)",
+          "IDEA(i)",
+          "HOLD(h)",
+          "REVW(r)",
+          "|",
+          "DONE(d)",
+          "REJECTED(r)",
+          "WAITING(w)",
+        },
+        org_capture_templates = {
+          t = {
+            description = "Refile",
+            template = "* TODO %?\nDEADLINE: %T",
+          },
+          T = {
+            description = "Todo",
+            template = "* TODO %?\nDEADLINE: %T",
+            target = "~/Documents/org/todos.org",
+          },
+          w = {
+            description = "Work todo",
+            template = "* TODO %?\nDEADLINE: %T",
+            target = "~/Documents/org/work.org",
+          },
+        },
+      })
+      require("org-bullets").setup({
+        concealcursor = true,
+        symbols = {
+          checkboxes = {
+            half = { "", "@org.checkbox.halfchecked" },
+            done = { "✓", "@org.checkbox.checked" },
+            todo = { " ", "@org.checkbox" },
+          },
+        },
+      })
+    end,
+  },
 }
