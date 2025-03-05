2ff85165ba4702a31b6d7458feaf9342540d988e
Jason Heppler <jason.heppler@gmail.com>
2025-03-05 10:42:38 -0600

    adaptable color scheme based on light/dark mode

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,49 @@
 return {
-  "EdenEast/nightfox.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme nightfox")
-  end,
+  -- {
+  --   "catppuccin/nvim",
+  -- },
+  -- {
+  --   "folke/tokyonight.nvim",
+  -- },
+  -- {
+  --   "nyoom-engineering/oxocarbon.nvim",
+  -- },
+  -- {
+  --   "zortax/three-firewatch",
+  -- },
+  -- {
+  --   "rakr/vim-two-firewatch",
+  -- },
+  { "miikanissi/modus-themes.nvim", priority = 1000 },
+  {
+    "vim-paper",
+    name = "vim-paper",
+    dir = "~/projects/vim-paper",
+  },
+  {
+    "Shatur/neovim-ayu",
+    priority = 1000,
+  },
+  {
+    "ribru17/bamboo.nvim",
+    priority = 1000,
+  },
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = vim.fn.getenv("ITERM_PROFILE") == "Light" and "paper" or "modus",
+    },
+  },
+  -- {
+  -- "EdenEast/nightfox.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd("colorscheme nightfox")
+  --   end,
+  -- }
 }
