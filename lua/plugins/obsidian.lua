a470c8355dbf88945cfba89c4baac465bb28470d
Jason Heppler <jason.heppler@gmail.com>
2025-07-21 07:56:40 -0500

    Use setup({}) properly in config

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,48 +1,46 @@
 -- obsidian
 return {
   "obsidian-nvim/obsidian.nvim",
-  version = "*", -- recommended, use latest release instead of latest commit
+  version = "*",
   ft = "markdown",
   dependencies = {
     "hrsh7th/nvim-cmp",
     "nvim-lua/plenary.nvim",
-    "Saghen/blink.cmp",
   },
-  opts = {
-    workspaces = {
-      {
-        name = "research",
-        path = "~/research/Research",
+  config = function()
+    require("obsidian").setup({
+      workspaces = {
+        {
+          name = "research",
+          path = vim.fn.expand("~/research/Research"),
+        },
       },
-    },
-  },
-  completion = {
-    -- Set to false to disable completion.
-    nvim_cmp = true,
-    -- Trigger completion at 2 chars.
-    min_chars = 2,
-  },
-  mappings = {
-    -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-    ["gf"] = {
-      action = function()
-        return require("obsidian").util.gf_passthrough()
-      end,
-      opts = { noremap = false, expr = true, buffer = true },
-    },
-    -- Toggle check-boxes.
-    ["<leader>ch"] = {
-      action = function()
-        return require("obsidian").util.toggle_checkbox()
-      end,
-      opts = { buffer = true },
-    },
-    -- Smart action depending on context: follow link, show notes with tag, or toggle checkbox.
-    ["<cr>"] = {
-      action = function()
-        return require("obsidian").util.smart_action()
-      end,
-      opts = { buffer = true, expr = true },
-    },
-  },
+      completion = {
+        nvim_cmp = true,
+        min_chars = 2,
+        match_case = false,
+        blink = false,
+      },
+      mappings = {
+        ["gf"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        ["<leader>ch"] = {
+          action = function()
+            return require("obsidian").util.toggle_checkbox()
+          end,
+          opts = { buffer = true },
+        },
+        ["<cr>"] = {
+          action = function()
+            return require("obsidian").util.smart_action()
+          end,
+          opts = { buffer = true, expr = true },
+        },
+      },
+    })
+  end,
 }
