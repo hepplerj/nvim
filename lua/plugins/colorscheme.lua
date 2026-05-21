227adcba1c38d55ec6d94383926d745188bee506
Jason Heppler <jason.heppler@gmail.com>
2026-05-20 21:52:53 -0500

    tweaks

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -14,4 +14,20 @@ return {
       require("modes").setup()
     end,
   },
+  {
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      update_interval = 5000,
+      set_dark_mode = function()
+        vim.cmd("colorscheme catppuccin-mocha")
+        vim.api.nvim_set_option_value("background", "dark", {})
+      end,
+      set_light_mode = function()
+        -- dayfox as secondary
+        -- or paper
+        vim.cmd("colorscheme dayfox")
+        vim.api.nvim_set_option_value("background", "light", {})
+      end,
+    },
+  },
 }
