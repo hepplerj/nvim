4cc9ff48fcf5e9304c5ab67aba6353f5c9c49522
Jason Heppler <jason.heppler@gmail.com>
2026-07-09 15:17:34 -0500

    fix find in files keybind

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -2,6 +2,9 @@ return {
   "ibhagwan/fzf-lua",
   -- optional for icon support
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+    { "<leader>fp", function() require("fzf-lua").live_grep() end, desc = "Find in files (live grep)" },
+  },
   config = function()
     require("fzf-lua").setup({ sort_lastused = true })
   end,
